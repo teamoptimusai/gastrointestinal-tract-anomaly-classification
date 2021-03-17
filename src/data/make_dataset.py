9e64f522bb67341f675931b9deea2f8c9b1c423d
Chandra Irugalbandara <irugalbandarachandra@gmail.com>
2021-03-18 04:38:43 +0530

    added make_dataset script

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,12 +1,33 @@
 import os
+import wget
+import shutil
+import argparse
+import glob
 
 def download_dataset(dataset = "Kvasir_Capsule"):  
     if dataset == "Kvasir_Capsule":
-        os.system(f'cmd /k "!wget -O "labelled_images.zip" https://files.osf.io/v1/resources/dv2ag/providers/googledrive/labelled_images/?zip="')
+        print("Downloading Kvasir Capsule Dataset from\n 'https://files.osf.io/v1/resources/dv2ag/providers/googledrive/labelled_images/?zip='")
+        wget.download('https://files.osf.io/v1/resources/dv2ag/providers/googledrive/labelled_images/?zip=', './data/raw/labelled_images.zip')
     elif dataset == "Hyper_Kvasir":
-        os.system(f'cmd /k "!wget https://datasets.simula.no/hyper-kvasir/hyper-kvasir-labeled-images.zip"')
+        None
     elif dataset == "Kvasir":
         None
 
-def make_dataset(dataset, download_dir = "./data/raw"):
-    None
+def make_dataset(dataset):
+    download_dataset(dataset)
+    if dataset == "Kvasir_Capsule":
+      print("Unpacking the Dataset and Preparing")
+      shutil.unpack_archive("./data/raw/labelled_images.zip", "./data/raw/kvasir_capsule_dataset")
+      os.remove("./data/raw/labelled_images.zip")
+      for tar_file in glob.glob("./data/raw/kvasir_capsule_dataset/*.gz"):
+        print(tar_file)
+        shutil.unpack_archive(tar_file, f"./data/raw/kvasir_capsule_dataset/{tar_file[34:-7]}")
+        os.remove(tar_file)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Download and Prepare Datasets')
+    parser.add_argument('--dataset', required=True, type=str)
+    args = parser.parse_args()
+
+    make_dataset(args.dataset)
